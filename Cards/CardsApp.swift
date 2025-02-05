91a9d651de0d6bec7656acbee8c1542ddd066c03
Mikkela Wilson <lovelivelife7474@gmail.com>
2025-02-05 18:09:12 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Mikkela Wilson on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
